15636fb2303c21bf9c9bed7362d3c5e8ccb241a7
Josiah <josiahblaisdell@outlook.com>
2018-02-09 20:20:50 -0600

    checkin functions defined in paper

--- sim.c
+++ sim.c
@@ -1,17 +1,30 @@
 #include <stdio.h>
 #include <sim.h>
 
-
-
-
 int main(){
     //N+2 = 7 => N=5
     static size = 49;
     //what are these statics? 
     //shouldn't "static" just limit the scope of the variable?
-    static u[size], u_prev[size], v_prev[size];
-    static dens[size];
 
+    // In this context (in main), "static" will have no meaningful effect.
+    // In the function "f" below, the static variable x will stay around after the function call, so that it increments with each call.
+    // For local variables like these, static means that the variable doesn't live in the function's "stack" 
+    // (temporary memory that is created for a function call and thrown away when the call completes)
+    // but lives outside in memory allocated to the program itself.  
+    // However, its scope is still limited to the local context.  So you couldn't access these variable outside main().
+    // Since main() is not a function you're going to call anywhere, static doesn't have any practical effect here.
 
-    return 0;
+    // Also, I'm surprised C lets you get away without specifying the data type of these variables.  Does this compile?
+
+    static float u[size], u_prev[size], v_prev[size];
+    static float dens[size];
+    u = u[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+    int x = 1;
+    return x;
+}
+
+void f() {
+    static int x = 0; // the initialization will only happen once.
+    ++x; // x will continuously increment with each call to f().
 }
--- sim.h
+++ sim.h
@@ -1,8 +1,13 @@
 #include <stdio.h>
+#define SWAP(x,y){ float* tmp = y; y=x; x=tmp; }
 #define IX(i,j) ((i)+(7)*(j))
+
 void add_source(int, float*, float*, float);
 void diffuse( int, int, float*, float*, float, float);
 void advect(int, int, float*, float*, float*, float*, float);
+void project(int, float*, float*, float*, float* );
+void dens_step(int, float*, float*, float*, float*, float, float);
+void set_bnd(int, int, float*);
 
 //Adds the source to the density
 void add_source(int N, float* x, float* s, float dt){
@@ -12,7 +17,7 @@ void add_source(int N, float* x, float* s, float dt){
     }
 }
 
-void diffuse(int N, float* x, float* x0, float diff, float dt){
+void diffuse(int N, int b, float* x, float* x0, float diff, float dt){
     int i,j,k;
     float a = dt*diff*N*N;
     for(k=0; k<20; k++){
@@ -49,4 +54,73 @@ void advect(int N, int b, float* d, float* d0, float* u, float* v, float dt){
         }
     }
     set_bnd(N,b,d);
+}
+
+void dens_step(int N, float* x, float* x0, float* u, float* v, float diff, float dt){
+    add_source(N,x,x0,dt);
+    SWAP(x0,x);
+    diffuse(N,0,x,x0,diff,dt);
+    SWAP(x0,x); 
+    advect(N,0,x,x0,u,v,dt);
+
+}
+
+void vel_step(int N, float* u, float* v, float* u0, float* v0, float visc, float dt){
+    add_source(N,u,u0,dt);
+    add_source(N,v,v0,dt);
+    SWAP(u0,u);
+    diffuse(N,1,u,u0,visc,dt);
+    SWAP(v0,v);
+    diffuse(N,2,v,v0,visc,dt);
+    project(N,u,v,u0,v0);
+    SWAP(u0,u);
+    SWAP(v0,v);
+    advect(N,1,u,u0,u0,v0,dt);
+    advect(N,2,v,v0,u0,v0,dt);
+}
+
+void project(int N, float* u, float* v, float* p, float* div){
+    int i,j,k;
+    float h;
+    h=1.0/N;
+    for(i=1;i<=N;i++){
+        for(j=1;j<=N;j++){
+            div[IX(i,j)]=-0.5*h*(u[IX(i+1,j)]-u[IX(i-1,j)]
+                                 + v[IX(i,j+1)]-v[IX(i,j-1)]);
+            p[IX(i,j)]=0;
+        }
+    }
+    set_bnd(N,0,div);
+    set_bnd(N,0,p);
+    for(k=0;k<20;k++){
+        for(i=1;i<=N;i++){
+            for(j=1; j<=N;j++){
+                p[IX(i,j)]=(div[IX(i,j)]+p[IX(i-1,j)]+p[IX(i+1,j)]
+                            + p[IX(i,j-1)] + p[IX(i,j+1)])/4;
+            }
+        }
+    set_bnd(N,0,p);
+    }
+    for(i=1;i<=N;i++){
+        for(j=1; j<= N; j++){
+            u[IX(i,j)]-= 0.5*(p[IX(i+1,j)]-p[IX(i-1,j)])/h;
+            u[IX(i,j)]-= 0.5*(p[IX(i,j+1)]-p[IX(i,j-1)])/h;
+        }
+    }
+    set_bnd(N,1,u);
+    set_bnd(N,2,v);
+}
+
+void set_bnd(int N, int b, float* x){
+    int i;
+    for(i=1;i<=N;i++){
+        x[IX(0,i)]   = b==1 ? -x[IX(1,i)] : x[IX(1,i)];
+        x[IX(N+1,i)] = b==1 ? -x[IX(N,i)] : x[IX(N,i)];
+        x[IX(i,0)]   = b==2 ? -x[IX(i,1)] : x[IX(i,1)];
+        x[IX(i,N+1)] = b==2 ? -x[IX(i,N)] : x[IX(i,N)]; 
+    }
+    x[IX(0,0)]     = 0.5*(x[IX(1,0)]   + x[IX(0,1)]);
+    x[IX(0,N+1)]   = 0.5*(x[IX(1,N+1)] + x[IX(0,N)]);
+    x[IX(N+1,0)]   = 0.5*(x[IX(N,0)]   + x[IX(N+1,1)]);
+    x[IX(N+1,N+1)] = 0.5*(x[IX(N,N+1)] + x[IX(N+1,N)]);
 }
